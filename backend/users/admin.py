49b9965f20cf94a9884c657bd7198c4c951c81a9
leonid Tsybulsky <axonyx@mail.ru>
2023-12-28 00:38:04 +0300

    Refactoring

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,11 +1,35 @@
 from django.contrib import admin
 
-from users.models import CustomUser
+from users.models import Subscription, CustomUser
 
 
 @admin.register(CustomUser)
-class IngredientAdmin(admin.ModelAdmin):
+class CustomUserAdmin(admin.ModelAdmin):
     list_display = (
+        'pk',
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+    )
+    search_fields = (
         'username',
         'email',
     )
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'author')
+    search_fields = (
+        'author__username',
+        'author__email',
+        'user__username',
+        'user__email',
+    )
+    list_filter = (
+        'author__username',
+        'user__username',
+    )
+    empty_value_display = '-пусто-'
